3d1e60f5c39e7fec5d901707b3744516317fc0a0
Stephen Leander <stephen.leander1@gmail.com>
2022-06-06 11:54:41 +0200

    Add Home content & initial styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Home.css';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div id="home">
+      <div className="homeTitle1">
+        Our favorite places are full of surprises...
+      </div>
+      <div className="homeTitle2">The right gears means everything.</div>
+      <div className="homeTitle3">Be prepared for any challenge.</div>
+      <img
+        src={require('../images/todd-diemer-fpNBYsymggk-unsplash.jpg')}
+      ></img>
+      <Link to="/shop">Shop now</Link>
     </div>
   );
 };
 
+// Photo by Todd Diemer on Unsplash
 export default Home;
