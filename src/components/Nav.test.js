94187f51cd3dc9516e26f4ac758ae92576f1feec
Stephen Leander <stephen.leander1@gmail.com>
2022-06-06 13:41:45 +0200

    Create tests for Nav

---
+++ src/components/Nav.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './Nav';
+
+describe('Nav component', () => {
+  it('renders h1 and Link div', () => {
+    const { nav } = render(
+      <BrowserRouter>
+        <Nav />
+      </BrowserRouter>
+    );
+    expect(nav).toMatchSnapshot();
+  });
+
+  it('renders correct title', () => {
+    const { getByRole } = render(
+      <BrowserRouter>
+        <Nav />
+      </BrowserRouter>
+    );
+    expect(getByRole('heading').textContent).toMatch(/outdoor store/i);
+  });
+
+  it('renders 3 Links', () => {
+    const { getAllByRole } = render(
+      <BrowserRouter>
+        <Nav />
+      </BrowserRouter>
+    );
+    expect(getAllByRole('link').length).toBe(3);
+  });
+
+  it('renders Links with correct names', () => {
+    const { getAllByRole } = render(
+      <BrowserRouter>
+        <Nav />
+      </BrowserRouter>
+    );
+    expect(getAllByRole('link')[0].textContent).toMatch(/home/i);
+    expect(getAllByRole('link')[1].textContent).toMatch(/shop/i);
+    expect(getAllByRole('link')[2].textContent).toMatch(/about/i);
+  });
+});
