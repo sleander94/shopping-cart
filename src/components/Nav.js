3b85ec434b103d0def137142be2f76fd17ec449c
Stephen Leander <stephen.leander1@gmail.com>
2022-06-06 13:24:18 +0200

    Refactor to only handle navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../styles/Nav.css';
 
 const Nav = () => {
   return (
-    <div id="page">
-      <nav id="nav">
-        <div className="title">Outdoor Store</div>
-        <div className="links">
-          <Link to="/home">Home</Link>
-          <Link to="/shop">Shop</Link>
-          <Link to="/about">About</Link>
-        </div>
-      </nav>
-      <div className="content">
-        <Outlet />
+    <nav id="nav">
+      <h1 className="title">Outdoor Store</h1>
+      <div className="links">
+        <Link to="/home">Home</Link>
+        <Link to="/shop">Shop</Link>
+        <Link to="/about">About</Link>
       </div>
-    </div>
+    </nav>
   );
 };
 
