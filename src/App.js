783433e8d4770ed771421f56cef6550bda940608
Stephen Leander <stephen.leander1@gmail.com>
2022-06-06 13:25:01 +0200

    Render App at root instead of RouteSwitch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Shopping Cart</h1>
-    </div>
-  );
-}
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './components/Main';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Shop from './components/Shop';
+import About from './components/About';
 
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route path="home" element={<Home />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 export default App;
