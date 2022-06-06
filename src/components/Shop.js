32935de09f6a4fb25d013d9dfed7e2ee4ea0d1a8
Stephen Leander <stephen.leander1@gmail.com>
2022-06-06 15:26:30 +0200

    Import & display products

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Product from './Product';
+import productList from '../products';
 
 const Shop = () => {
   return (
-    <div>
+    <div id="shop">
       <h1>Shop</h1>
+      <div id="products">
+        {productList.map((product) => (
+          <Product key={productList.indexOf(product)} {...product} />
+        ))}
+      </div>
     </div>
   );
 };
