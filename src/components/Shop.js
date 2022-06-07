705644790cd21f0fdba40480184265da5097d2dd
Stephen Leander <stephen.leander1@gmail.com>
2022-06-07 11:03:23 +0200

    Impport and display Cart, update Product renders to handle quantity

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Product from './Product';
+import Cart from './Cart';
 import productList from '../products';
 import '../styles/Shop.css';
 
@@ -13,25 +14,29 @@ const Shop = () => {
       if (product.name === item.name) {
         item.quantity += 1;
         added = true;
+        // Updating state here to trigger rerender to show quantity change
+        setCartProducts([...cartProducts]);
       }
     });
-
     if (added === false) {
       product.quantity = 1;
       setCartProducts([...cartProducts, product]);
     }
-    console.log(cartProducts);
   };
   return (
     <div id="shop">
-      {productList.map((product) => (
-        <Product
-          key={productList.indexOf(product)}
-          {...product}
-          index={productList.indexOf(product)}
-          addToCart={addToCart}
-        />
-      ))}
+      <div className="products">
+        {productList.map((product) => (
+          <Product
+            key={productList.indexOf(product)}
+            {...product}
+            quantity={null}
+            index={productList.indexOf(product)}
+            addToCart={addToCart}
+          />
+        ))}
+      </div>
+      <Cart cartProducts={cartProducts} />
     </div>
   );
 };
