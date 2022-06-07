7bae25757f674450fad00ae0d09a96300bbcbded
Stephen Leander <stephen.leander1@gmail.com>
2022-06-07 11:02:26 +0200

    Pass quantity prop to Product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../styles/Product.css';
 
-const Product = ({ name, price, image, alt, index, addToCart }) => {
+const Product = ({ name, price, image, alt, index, quantity, addToCart }) => {
   return (
     <div className="product">
       <img src={image} alt={alt}></img>
       <div className="name">{name}</div>
       <div className="price">${price}</div>
+      {quantity && <div>{quantity}</div>}
       <button className="addToCart" onClick={() => addToCart(index)}>
         Add to cart
       </button>
