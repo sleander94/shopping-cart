72c382226f323ed405713f95236f150a7392bc55
Stephen Leander <stephen.leander1@gmail.com>
2022-06-07 16:42:35 +0200

    Initial products commit

---
+++ src/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'Four Person Camping Tent',
+    price: '349.99',
+    image: require('./images/4tent.jpeg'),
+    alt: 'green tent',
+  },
+  {
+    name: 'Six Person Luxury Tent',
+    price: '499.99',
+    image: require('./images/6tent.jpeg'),
+    alt: 'orange and blue tent',
+  },
+  {
+    name: 'Long Range Binoculars',
+    price: '219.99',
+    image: require('./images/binoculars.jpeg'),
+    alt: 'black binoculars',
+  },
+  {
+    name: 'Sub-Zero Sleeping Bag',
+    price: '99.99',
+    image: require('./images/sleepingbag.jpeg'),
+    alt: 'yellow and black sleeping bag',
+  },
+  {
+    name: 'Double Sleeping Bag',
+    price: '165.49',
+    image: require('./images/doublebag.jpeg'),
+    alt: 'orange and blue double sleeping bag',
+  },
+  {
+    name: 'Weather Proof Handhelf GPS',
+    price: '699.99',
+    image: require('./images/gps.jpeg'),
+    alt: 'gps displaying color map',
+  },
+  {
+    name: 'High Visibility Puffer Jacket',
+    price: '79.95',
+    image: require('./images/jacket.jpeg'),
+    alt: ' red jacket',
+  },
+  {
+    name: 'Mens Trekking Pack 60L',
+    price: '219.99',
+    image: require('./images/mbackpack.jpeg'),
+    alt: 'beige trekking backpack',
+  },
+  {
+    name: 'Womens Trekking Pack 60L',
+    price: '229.99',
+    image: require('./images/wbackpack.jpeg'),
+    alt: 'teal trekking backpack',
+  },
+  {
+    name: 'Stainless Steel Multitool',
+    price: '149.99',
+    image: require('./images/multitool.jpeg'),
+    alt: 'multitool displaying pliers and knives',
+  },
+  {
+    name: 'Carbon Fiber Trekking Poles',
+    price: '199.99',
+    image: require('./images/poles.jpeg'),
+    alt: 'extendable trekking poles with wrist straps',
+  },
+  {
+    name: 'Portable Gas Camp Stove',
+    price: '89.99',
+    image: require('./images/stove.jpeg'),
+    alt: 'double burner red camp stove',
+  },
+];
+
+export default products;
