2d1dc9c610c9a4167486d874736014fcbb7d846f
Stephen Leander <stephen.leander1@gmail.com>
2022-06-08 15:48:16 +0200

    Add cart labels & refactor for styling

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,41 +1,69 @@
 import React, { useState } from 'react';
 import CartProduct from './CartProduct';
+import icon from '../images/cart.svg';
 import '../styles/Cart.css';
 
 const Cart = ({ cartProducts, changeQuantity }) => {
   const [isShown, setIsShown] = useState(false);
+  const [active, setActive] = useState('');
 
   const showCart = () => {
     if (!isShown) {
       setIsShown(true);
+      setActive('active');
     } else {
       setIsShown(false);
+      setActive('');
     }
   };
 
   let total = 0;
+  let totalProducts = 0;
   cartProducts.forEach((product) => {
     total += product.price * product.quantity;
+    totalProducts += product.quantity;
   });
 
   return (
     <div id="cart">
-      <button id="cartButton" onClick={showCart}>
-        Cart
-      </button>
+      <div id="cartButton" onClick={showCart} className={active}>
+        <img src={icon} alt="shopping cart"></img>
+        <div className="totalProducts">{totalProducts}</div>
+      </div>
       {isShown && (
-        <div id="cartDisplay">
-          {cartProducts
-            .filter((product) => product.quantity > 0)
-            .map((product) => (
-              <CartProduct
-                key={cartProducts.indexOf(product)}
-                {...product}
-                index={cartProducts.indexOf(product)}
-                changeQuantity={changeQuantity}
-              />
-            ))}
-          <div className="total">Total: ${total}</div>
+        <div id="cartContainer">
+          <div id="cartDisplay">
+            <h3 className="title">Your Cart</h3>
+            <div className="cartLabels">
+              <div className="name">Item</div>
+              <div className="quantity">Qty.</div>
+              <div className="price">Price</div>
+            </div>
+            <div id="cartProducts">
+              {cartProducts.length < 1 && (
+                <div className="empty">
+                  Your cart is empty! Add some items to checkout.
+                </div>
+              )}
+              {cartProducts
+                .filter((product) => product.quantity > 0)
+                .map((product) => (
+                  <CartProduct
+                    key={cartProducts.indexOf(product)}
+                    {...product}
+                    index={cartProducts.indexOf(product)}
+                    changeQuantity={changeQuantity}
+                  />
+                ))}
+            </div>
+            <div id="checkoutContainer">
+              <div className="close" onClick={showCart}>
+                Close
+              </div>
+              <div className="checkout">Checkout</div>
+              <div className="total">Total: ${total.toFixed(2)}</div>
+            </div>
+          </div>
         </div>
       )}
     </div>
