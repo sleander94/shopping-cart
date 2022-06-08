5027772216256e3ac99af34118e3ba699df343c4
Stephen Leander <stephen.leander1@gmail.com>
2022-06-08 15:48:40 +0200

    Add footer

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Nav from './Nav';
+import icon from '../images/iconmonstr-github-1.svg';
 import '../styles/Main.css';
 
 const Main = () => {
@@ -9,6 +10,10 @@ const Main = () => {
       <Nav />
       <div id="content">
         <Outlet />
+      </div>
+      <div id="footer">
+        <div>sleander94</div> <img src={icon} alt="github logo"></img>
+        <div>2022</div>
       </div>
     </div>
   );
