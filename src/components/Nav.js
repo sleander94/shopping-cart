482e23f5e83dd451d51f9df60db5b410ff8d189f
Stephen Leander <stephen.leander1@gmail.com>
2022-06-08 20:22:57 +0200

    Add home link to nav title

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,9 @@ import '../styles/Nav.css';
 const Nav = () => {
   return (
     <nav id="nav">
-      <h1 className="title">Outdoor Store</h1>
+      <Link to="/home">
+        <h1 className="title">Outdoor Store</h1>
+      </Link>
       <div className="links">
         <Link to="/home">Home</Link>
         <Link to="/shop">Shop</Link>
