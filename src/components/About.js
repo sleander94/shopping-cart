d845c0cbfbe16db33d4d3d419baf8d62bc8bcf3b
Stephen Leander <stephen.leander1@gmail.com>
2022-06-08 15:49:57 +0200

    Import and display icons

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import github from '../images/iconmonstr-github-1.svg';
+import instagram from '../images/iconmonstr-instagram-11.svg';
+import facebook from '../images/iconmonstr-facebook-1.svg';
+import twitter from '../images/iconmonstr-twitter-1.svg';
+import '../styles/About.css';
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div id="about">
+      <div className="icons">
+        <img src={github} alt="github"></img>
+        <img src={instagram} alt="instagram"></img>
+        <img src={facebook} alt="facebook"></img>
+        <img src={twitter} alt="twitter"></img>
+      </div>
     </div>
   );
 };
